d8029449174766a86b982f8f21c5a07714e1c94c
akk98 <sunnyboy211@gmail.com>
2020-02-17 17:48:06 +0800

    發布項目spring

---
+++ 1_spring/src/abcc/Test.java
@@ -0,0 +1,13 @@
+package abcc;
+
+public class Test{
+	   public static void main(String args[]){
+	      String Str = new String("123456789ABCDEF");
+
+	      System.out.print("Return Value :" );
+	      System.out.println(Str.subSequence(1, 10) );
+
+	      System.out.print("Return Value :" );
+	      System.out.println(Str.subSequence(10, 15) );
+	   }
+	}
