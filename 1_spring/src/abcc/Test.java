0fb7bcf193c265c286c01800438519ca13ada907
sunnyboy211 <sunnyboy211@gmail.com>
2020-02-17 18:40:08 +0800

    dddd

--- 1_spring/src/abcc/Test.java
+++ 1_spring/src/abcc/Test.java
@@ -3,7 +3,7 @@ package abcc;
 public class Test{
 	   public static void main(String args[]){
 	      String Str = new String("123456789ABCDEF");
-
+	      //12333
 	      System.out.print("Return Value :" );
 	      System.out.println(Str.subSequence(1, 10) );
 
